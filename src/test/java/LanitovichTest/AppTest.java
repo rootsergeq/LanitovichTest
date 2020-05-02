0d41ffbb7712bc9794d3c874ceb5bbc716a5f28e
Sergeq <sergeq13@yandex.ru>
2020-05-02 12:54:46 +0300

    НЕ проходит проверку!

--- src/test/java/LanitovichTest/AppTest.java
+++ src/test/java/LanitovichTest/AppTest.java
@@ -1,9 +1,13 @@
 package LanitovichTest;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,22 +22,31 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
+
+
         System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
         Configuration.startMaximized = true;
-        Configuration.timeout = 5000;
+
+        Configuration.timeout = 10000;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-save-password-bubble");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        WebDriverRunner.setWebDriver(driver);
+
         open("https://dev.n7lanit.ru/");
         //шаг первый
 
         $(By.xpath("//button[@class='btn navbar-btn btn-default btn-sign-in']")).click();
-        $(By.xpath("//input[@id='id_username']")).sendKeys("rootsergeq");
-        $(By.xpath("//input[@id='id_password']")).sendKeys("2662645q");
-        $(By.xpath("//button[@class='btn btn-primary btn-block']")).click();
+        $(By.xpath("//input[@id='id_username']")).sendKeys("rootsergeqq");
+        $(By.xpath("//input[@id='id_password']")).sendKeys("123456789qq");
+        $(By.xpath("//button[@class =\"btn btn-primary btn-block\"]")).click();
         // Шаг второй
         Assert.assertTrue("логин не был выполнен", $(By.xpath("//a[@class='dropdown-toggle']//img[@class='user-avatar']")).isDisplayed());
         // Шаг третий
-        Double m = Math.random();
-        //$(By.xpath("(//div[@class = 'row thread-row']//span[contains(text(),'ответ')])[" + m + "]"));
-
-        System.out.println(m);
+        //int a = (int) (Math.random() * (100 + 1)) - 98;
+        //$(By.xpath("(//div[@class = 'row thread-row']//span[contains(text(),'ответ')])[" + a + "]"));
+        //System.out.println(a);
+        //$(By.xpath("(//li[@class='list-group-item thread-new' and not(descendant::span[contains(text(), 'Опрос')])])[2]//div//a//span[2]")).click();
     }
 }
